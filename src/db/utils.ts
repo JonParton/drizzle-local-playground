c21caf72dd4ce871839b4c9c14866428baa4229d
Jonathan Parton <jon.parton@gmail.com>
2023-07-29 17:20:41 +0100

    add util function for json_agg's

---
+++ src/db/utils.ts
@@ -0,0 +1,40 @@
+import {
+  AnyColumn,
+  AnyTable,
+  InferColumnsDataTypes,
+  InferModel,
+  SQL,
+  TableConfig,
+  sql,
+} from "drizzle-orm";
+
+function jsonAggBuildObject<T extends Record<string, AnyColumn>>(shape: T) {
+  const chunks: SQL[] = [];
+  const filters: SQL[] = [];
+
+  Object.entries(shape).forEach(([key, value]) => {
+    if (chunks.length > 0) {
+      chunks.push(sql.raw(`,`));
+    }
+    chunks.push(sql.raw(`'${key}',`));
+    chunks.push(sql`${value}`);
+
+    if (filters.length > 0) {
+      filters.push(sql.raw(` or `));
+    }
+
+    filters.push(sql`${value} is not null`);
+  });
+
+  return sql<
+    InferColumnsDataTypes<T>[]
+  >`coalesce(json_agg(json_build_object(${sql.fromList(
+    chunks
+  )})) filter (where ${sql.fromList(filters)}), '[]')`;
+}
+
+export function jsonAgg<Table extends AnyTable<TableConfig>>(table: Table) {
+  return sql<
+    InferModel<Table>[]
+  >`coalesce(json_agg(${table}) filter (where ${table} is not null), '[]')`;
+}
